0979b83c06555491a04b26150ef874710326006b
Garrett <gj.gregory@gmail.com>
2016-12-19 15:23:42 -0800

    added card images (taken from web). added helper function to load card images into buttons.

--- play_holdem.py
+++ play_holdem.py
@@ -11,16 +11,44 @@ class PlayHoldem:
     def destroy(self, widget, data=None):
         gtk.main_quit()
 
+    def __set_image(self, widget, img_filename):
+        #change background color of widget
+        colormap = widget.get_colormap()
+        color = colormap.alloc_color("white")
+        style = widget.get_style().copy()
+        style.bg[gtk.STATE_NORMAL] = color
+        widget.set_style(style)
+
+        #initialize box and image
+        box = gtk.HBox(False, 0)
+        box.set_border_width(5)
+        image = gtk.Image()
+
+        #build and re-scale pixbuf
+        pixbuf = gtk.gdk.pixbuf_new_from_file(img_filename)
+        scaled_pixbuf = pixbuf.scale_simple(80, 120, gtk.gdk.INTERP_BILINEAR)
+
+        #set image into box and add box to button
+        image.set_from_pixbuf(scaled_pixbuf)
+        box.pack_start(image, False, False, 0)
+        image.show()
+        widget.add(box)
+        box.show()
+
+
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
         self.window.set_border_width(200)
 
-        self.button = gtk.Button("Play Game")
-        self.button.set_size_request(100,100)
-        self.window.add(self.button)
-        self.button.show()
+        self.card1 = gtk.Button()
+        self.__set_image(self.card1, 'art_assets/ace_of_spades.png')
+        self.card1.show()
+
+
+
+        self.window.add(self.card1)
         self.window.show()
 
     def main(self):
